add988d8311fcfd2c24fa652f6e5ea383735fc34
Usmounika <usmounikamanchala03@gmail.com>
2026-02-05 19:34:37 +0530

    palindrome

---
+++ src/test/java/CountVowels.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CountVowels {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        String str=scan.next();
+        int count=0;
+        for(int i=0;i<str.length();i++){
+       //     if(str.contains('a' || 'i'||'e'||'0'||'u'||'A'||'E'||'I'||'O'||'U')){
+       //         count++;
+        //    }
+        }
+        System.out.println("vowels count: " +count );
+
+    }
+}
